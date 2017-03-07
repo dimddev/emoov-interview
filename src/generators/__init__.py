c640709be8acb4e813839efa9c727ee077473930
dimd <targolini@gmail.com>
2017-03-07 14:55:04 +0200

    rework of generators task

--- src/generators/__init__.py
+++ src/generators/__init__.py
@@ -4,11 +4,33 @@ import itertools
 class Generators:
 
     def chain(self, iter1, iter2):
-        return list(iter1) + list(iter2)
+        """chain
+
+        :param iter1
+        :param iter2
+        """
+        for x in itertools.chain(iter1, iter2):
+            yield x
 
     def compress(self, iterable, mask):
-        return [x[0] for x in zip(iterable, mask) if all(x)]
+        """compress
 
-    def cycle(self, iter_data):
-        return itertools.cycle(iter_data)
+        :param iterable
+        :param mask
+        """
+        for y in [x[0] for x in zip(iterable, mask) if all(x)]:
+            yield y
+
+    def cycle(self, iter_data, samples=1000):
+        """cycle
+
+        :param iter_data
+        :param sample
+        """
+
+        for _cycle in itertools.cycle(iter_data):
+            yield _cycle
+            samples -= 1
+            if not samples:
+                break
 
